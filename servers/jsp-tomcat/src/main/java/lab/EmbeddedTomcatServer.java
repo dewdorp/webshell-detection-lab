1267545e50606692327e6f78d517bf72837c404d
dewdorp <53504838+dewdorp@users.noreply.github.com>
2026-05-21 07:04:42 +0000

    Allow external lab binding

--- servers/jsp-tomcat/src/main/java/lab/EmbeddedTomcatServer.java
+++ servers/jsp-tomcat/src/main/java/lab/EmbeddedTomcatServer.java
@@ -9,6 +9,7 @@ import org.apache.catalina.startup.Tomcat;
 public class EmbeddedTomcatServer {
     public static void main(String[] args) throws Exception {
         int port = Integer.parseInt(System.getProperty("lab.port", System.getenv().getOrDefault("LAB_PORT", "8080")));
+        String host = System.getProperty("lab.host", System.getenv().getOrDefault("LAB_HOST", "0.0.0.0"));
         File serverDir = new File(".").getCanonicalFile();
         File uploadDir = new File(serverDir, "uploads");
         File logDir = new File(serverDir, "logs");
@@ -16,13 +17,13 @@ public class EmbeddedTomcatServer {
         logDir.mkdirs();
         Tomcat tomcat = new Tomcat();
         tomcat.setPort(port);
-        tomcat.getConnector().setProperty("address", "127.0.0.1");
+        tomcat.getConnector().setProperty("address", host);
         Context context = tomcat.addContext("", serverDir.getAbsolutePath());
         Wrapper wrapper = Tomcat.addServlet(context, "uploadServlet", new UploadServlet(serverDir));
         wrapper.setMultipartConfigElement(new MultipartConfigElement(uploadDir.getAbsolutePath()));
         context.addServletMappingDecoded("/*", "uploadServlet");
         tomcat.start();
-        System.out.printf("JSP/Tomcat lab server listening on http://127.0.0.1:%d%n", port);
+        System.out.printf("JSP/Tomcat lab server listening on http://%s:%d%n", host, port);
         tomcat.getServer().await();
     }
 }
